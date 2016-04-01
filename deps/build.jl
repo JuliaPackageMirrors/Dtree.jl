2a9b0f49536f510356ea1e9c524b845f87325764
Kiran Pamnany <kiran.pamnany@intel.com>
2016-04-01 10:46:45 -0700

    Build with either Intel MPI or Open MPI

--- deps/build.jl
+++ deps/build.jl
@@ -5,8 +5,15 @@ vers = "1.0"
 
 if !isfile(target)
     if OS_NAME == :Linux
-	println("Compiling libdtree...")
-	run(`make`)
+        # TODO: clean this up to use Intel MPI if it's available
+        # and Open MPI otherwise
+        if gethostname()[1:4] == "cori"
+            println("Compiling libdtree with Intel MPI...")
+            run(`make`)
+        else
+            println("Compiling libdtree with OpenMPI...")
+            run(`make OPENMPI=1`)
+        end
     else
 	error("Dtree is Linux-only right now")
     end
